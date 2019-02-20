ab8917806d04dca527016d512a1c57b161a236f8
Chengcheng Mou <chengcheng@mail.usf.edu>
2019-02-19 21:50:15 -0500

    JavaScript

--- RiskControlGUI/app.js
+++ RiskControlGUI/app.js
@@ -1,17 +1,44 @@
+/**
+ # @Author: Chengcheng Mou <ccm>
+ # @Date:   02-19-2019
+ # @Email:  chengcheng@mail.usf.edu
+ # @Last modified by:   ccm
+ # @Last modified time: 02-19-2019
+ */
+
+
+
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 
-var controller = require('./controllers/controller')
+const port = 3000;
+
+//static files
+app.use(express.static('public'));
+
+app.use(bodyParser.urlencoded({ extended: true }));
 
 //set up template engine
 app.set('view engine', 'ejs');
 
-//static files
-app.use(express.static('./public'));
+
+app.get('/', function (req, res) {
+  res.render('index');
+})
+
+app.post('/', function (req, res) {
+  console.log(req.body.city);
+  res.render('index');
+})
+
+
+
 
 //fire controllers
 //controller(app);
 
 //listen to port
-const server = app.listen(8080);
-console.log(`You are listening to port ${server.address().port}`);
+app.listen(port, function () {
+  console.log(`Example app listening on port ${port}!`);
+})
--- RiskControlGUI/controllers/controller.js
+++ RiskControlGUI/controllers/controller.js
@@ -1,7 +1,1 @@
 var bodyParser = require('body-parser');
-
-//const D3Node = require('d3-node');
-const d3 = require('d3')
-
-const fs = require('fs');
-//const svg2png = require('svg2png');
